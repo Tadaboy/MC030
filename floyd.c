8ff7483125c98c9e82fedba644125d3e81bdd0eb
Tadaboy <Tadaboy@users.noreply.github.com>
2017-03-27 08:05:04 -0300

    Degree Sequence

---
+++ floyd.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+void floyd(int *matrix[], int *matrix2[], int nv);
+
+void print_floyd(int *matrix[], int nv);
+
+void checa_ciclos_negativos(int *matrix[], int num_vert);
+
+int main() {
+
+	// Le numero de nos e numero de arestas
+	int num_vert, num_arest, a1, a2, p, i, j;
+
+	printf("Digite o numero de vertices e de arestas\n");
+	scanf("%d %d", &num_vert, &num_arest);
+
+	/*Criação de matrizes quadradas nxn*/
+
+	int **dist = (int**) malloc (num_vert*sizeof(int*));
+	int **pi = (int**) malloc (num_vert*sizeof(int*));	
+		
+	for(i = 0; i < num_vert; i++){
+        dist[i] = (int*) malloc (num_vert*sizeof(int));
+        pi[i] = (int*) malloc (num_vert*sizeof(int));
+        for(j = 0; j < num_vert; j++){
+            dist[i][j] = 1000;
+            pi[i][j] = -1;
+            if(i == j){
+                dist[i][j] = 0;
+                pi[i][j] = i;
+            }
+        }
+	}
+	
+	
+    /*Leitura de arestas*/
+    
+    print_floyd(dist, num_vert);
+    print_floyd(pi, num_vert);
+
+    printf("Digite as arestas (A->B) Peso\n");
+    printf("No formato: A B P\n");
+    for(i = 0; i < num_arest; i++){
+        scanf("%d %d %d", &a1, &a2, &p);
+        dist[a1][a2] = p;
+        pi[a1][a2] = a1;
+    }
+
+    printf("Grafo inicial:\n");
+    print_floyd(dist, num_vert);
+
+    floyd(dist, pi, num_vert);
+
+    printf("Matriz de distancias:\n");
+    print_floyd(dist, num_vert);
+
+    printf("Matriz de predecessores:\n");
+    print_floyd(pi, num_vert);
+
+    checa_ciclos_negativos(dist, num_vert);
+
+	free(dist);
+	free(pi);
+    return 0;
+}
+
+
+void floyd(int *dist[], int*pi[], int nv){
+    int k, i, j;
+    for(k = 0; k < nv; k++){
+        for(i = 0; i < nv; i++){
+            for(j = 0; j < nv; j++){
+                if(dist[i][j] > dist[i][k] + dist[k][j]){
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                    pi[i][j] = pi[k][j];
+                }
+            }
+        }
+    }
+}
+
+void print_floyd(int *matrix[], int nv){
+    int i, j;
+    for(i = 0; i < nv; i++){
+        for(j = 0; j < nv; j++){
+            printf("%5d     ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void checa_ciclos_negativos(int *matrix[], int nv){
+    int i, k = 0;
+    for(i = 0; i < nv; i++){
+        if(matrix[i][i] < 0){
+            k = 1;
+        }
+    }
+    if(k == 1){
+        printf("Existe 1 ou mais ciclos negativos no grafo\n");
+    }
+    else{
+        printf("Nao existem ciclos negativos no grafo\n");
+    }
+}
---
+++ isomorphism-dsld.cpp
@@ -0,0 +1,168 @@
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void floyd(int *dist[], int nv){
+    int k, i, j;
+    for(k = 0; k < nv; k++){
+        for(i = 0; i < nv; i++){
+            for(j = 0; j < nv; j++){
+                if(dist[i][j] > dist[i][k] + dist[k][j]){
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+}
+
+int cmpInt (const void * a, const void * b) {
+	return ( *(int*)a - *(int*)b );
+}
+
+int checkDegrees (int **m1, int **m2, int n)
+{
+	int in1 [n];
+	int in2 [n];
+	int out1 [n];
+	int out2 [n];
+
+	//Inicialização (trocar por calloc)
+	for(int i = 0; i < n; i++) {
+		in1[i] = 0;
+		in2[i] = 0;
+		out1[i] = 0;
+		out2[i] = 0;
+	}
+	
+	//Cria vetores de in/out a partir da matriz de adj
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			int val1 = m1[i][j];
+			int val2 = m2[i][j];
+			
+			in1[j] += val1;
+			out1[i] += val1;
+			
+			in2[j] += val2;
+			out2[i] += val2;
+		}
+	}
+	
+	int classe1 [n];
+	int classe2 [n];
+	
+	//Inicialização (trocar por calloc)
+	for(int i = 0; i < n; i++)  {
+		classe1[i] = 0;
+		classe2[i] = 0;
+	}
+	
+	int cl_n = 0; //classe
+	int cl_i = in1[0];
+	int cl_o = out1[0];
+	
+	for(int i = 0; i < n; i++) {
+		int i1 = in1[i];
+		int o1 = out1[i];
+		//int i2 = in2[i];
+		//int o2 = out2[i];
+		
+		//Pareamento de Cantor
+		/*
+		classe1[i] = (i1 + o1)*(i1 + o1 + 1)/2 + o1;
+		classe2[i] = (i2 + o2)*(i2 + o2 + 1)/2 + o2;
+		*/
+		
+		 
+	}
+	
+	qsort(classe1, n, sizeof(int), cmpInt);
+	qsort(classe2, n, sizeof(int), cmpInt);
+	
+	for(int i = 0; i < n; i++) {
+		if(classe1[i] != classe2[i])
+			return 0;
+	}
+	return 1;
+}
+
+
+int main ()
+{
+	int n_vert, n_arest;
+	
+	cout << "Numero de vertices: ";
+	cin >> n_vert;
+	
+	cout << "Numero de arestas: ";
+	cin >> n_arest;
+	
+	//cout << "n_vert : " << n_vert << endl;
+	//cout << "n_arest : " << n_arest << endl;
+	
+	/*Aloca as matrizes*/
+	int **adj1 = new int* [n_vert];
+	int **adj2 = new int* [n_vert];
+	int **dist1 = new int* [n_vert];
+	int **dist2 = new int* [n_vert];
+	for(int i = 0; i < n_vert; i++) {
+		adj1[i] = new int [n_vert];
+		adj2[i] = new int [n_vert];
+	}
+	
+	
+	/*Zera as matrizes*/
+	for(int i = 0; i < n_vert; i++)
+		for(int j = 0; j < n_vert; j++) {
+			adj1[i][j] = 0;
+			adj2[i][j] = 0;
+		}
+			
+	/*Le as matrizes*/
+	int u,v;
+	for(int i = 0; i < n_arest; i++) {
+		cin >> u;
+		cin >> v;
+		adj1[u][v] = 1;
+	}
+	for(int i = 0; i < n_arest; i++) {
+		cin >> u;
+		cin >> v;
+		adj2[u][v] = 1;
+	}
+	
+	/*Imprime as matrizes*/
+	cout << "Matriz 1\n";
+	for(int i = 0; i < n_vert; i++) {
+		for(int j = 0; j < n_vert; j++)
+			cout << adj1[i][j] << " ";
+		cout << "\n";
+	}
+	cout << "\nMatriz 2\n";
+	for(int i = 0; i < n_vert; i++) {
+		for(int j = 0; j < n_vert; j++)
+			cout << adj2[i][j] << " ";
+		cout << "\n";
+	}
+	
+	int grau = checkDegrees(adj1, adj2, n_vert);
+	if(grau)
+		cout << "Os grafos possuem mesma sequencia de graus\n";
+	else {
+		cout << "Os grafos não são isomorfos\n";
+		return 0;
+	}
+	
+	
+	
+	/*Desaloca as matrizes*/
+	for(int i = 0; i < n_vert; i++) {
+		delete [] adj1[i];
+		delete [] adj2[i];
+	}
+	delete [] adj1;
+	delete [] adj2;
+	
+	return 0;
+}
